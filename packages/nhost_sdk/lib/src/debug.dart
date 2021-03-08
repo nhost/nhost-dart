befc4e8ee72794facfe8579f024a061b95500909
Scott Hyndman <scott@madewithfelt.com>
2021-03-08 18:12:54 -0500

    (nhost_sdk): Port basics of auth over from TS
    There are still some missing bits, like how to handle autoLogin in the
    absence of a URL, providers, and the ability to actually make HTTP
    calls.

---
+++ packages/nhost_sdk/lib/src/debug.dart
@@ -0,0 +1,11 @@
+void debugPrint(String message) {
+  print('DEBUG $message');
+}
+
+void warnPrint(String message) {
+  print('WARN  $message');
+}
+
+void errorPrint(String message) {
+  print('ERROR $message');
+}
