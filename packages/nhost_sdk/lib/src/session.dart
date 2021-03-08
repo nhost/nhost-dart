73bc7e15a451475df717d762665103e9bdc7fb72
Scott Hyndman <scott@madewithfelt.com>
2021-03-08 16:50:37 -0500

    (nhost_sdk): Add a higher level UserSession type
    This wraps a JWT token to expose claims.
    
    (Maybe this should be merged into the API session?)

---
+++ packages/nhost_sdk/lib/src/session.dart
@@ -0,0 +1,28 @@
+import 'package:jwt_decoder/jwt_decoder.dart';
+
+import 'api/auth_api.dart';
+
+const _hasuraClaimsNamespace = 'https://hasura.io/jwt/claims';
+
+class UserSession {
+  Session _session;
+  Map<String, dynamic> _hasuraClaims;
+
+  void clear() {
+    _session = null;
+    _hasuraClaims = null;
+  }
+
+  Session get session => _session;
+  set session(Session session) {
+    _session = session;
+
+    final decodedToken = JwtDecoder.decode(session.jwtToken);
+    _hasuraClaims =
+        decodedToken[_hasuraClaimsNamespace] as Map<String, dynamic>;
+  }
+
+  String getClaim(String claim) {
+    return _hasuraClaims != null ? _hasuraClaims[claim] : null;
+  }
+}
