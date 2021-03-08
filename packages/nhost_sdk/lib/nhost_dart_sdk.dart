f1e695291684ccadd5b6f348c549e574959d1c38
Scott Hyndman <scott@madewithfelt.com>
2021-03-08 18:14:10 -0500

    (nhost_sdk): Add NhostClient object for easy instantiation of sub-APIs

--- packages/nhost_sdk/lib/nhost_dart_sdk.dart
+++ packages/nhost_sdk/lib/nhost_dart_sdk.dart
@@ -1,4 +1,39 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library nhost_dart_sdk;
+
+import 'package:meta/meta.dart';
+
+import 'src/auth.dart';
+import 'src/client_storage.dart';
+import 'src/session.dart';
+
+class NhostClient {
+  NhostClient({
+    @required this.baseUrl,
+    bool autoLogin = true,
+    ClientStorage clientStorage,
+    Duration tokenRefreshInterval,
+  })  : assert(
+            baseUrl != null,
+            'Please specify a baseURL. More information at '
+            // TODO(shyndman): URL for Dart required
+            'https://docs.nhost.io/libraries/nhost-dart-sdk#setup'),
+        _autoLogin = autoLogin,
+        _session = UserSession(),
+        _refreshInterval = tokenRefreshInterval,
+        clientStorage = clientStorage ?? InMemoryClientStorage();
+
+  final String baseUrl;
+  final ClientStorage clientStorage;
+  final Duration _refreshInterval;
+  final bool _autoLogin;
+  final UserSession _session;
+
+  Auth get auth => _auth ??= Auth(
+        baseUrl: baseUrl,
+        autoLogin: _autoLogin,
+        clientStorage: clientStorage,
+        refreshInterval: _refreshInterval,
+        session: _session,
+      );
+  Auth _auth;
+}
