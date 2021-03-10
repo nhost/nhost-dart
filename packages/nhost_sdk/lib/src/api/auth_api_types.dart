10181d5922344c0bc4369c3d64288e0ae88292ca
Scott Hyndman <scott@madewithfelt.com>
2021-03-09 21:05:14 -0500

    (nhost_sdk): Clean up and document code in the api package

--- packages/nhost_sdk/lib/src/api/auth_api_types.dart
+++ packages/nhost_sdk/lib/src/api/auth_api_types.dart
@@ -6,6 +6,10 @@ import 'core_codec.dart';
 
 part 'auth_api_types.g.dart';
 
+/// Describes the client's authorization state.
+///
+/// Many authorization-related API calls return instances of this class. The
+/// details of which can be found on specific methods of [Auth].
 class AuthResponse {
   AuthResponse({this.session, this.user, this.mfa});
   final Session session;
@@ -22,6 +26,7 @@ class AuthResponse {
   }
 }
 
+/// Represents a user-authenticated session with Nhost.
 @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
 class Session {
   Session({
@@ -32,13 +37,24 @@ class Session {
     this.mfa,
   });
 
+  /// The raw JSON web token
   final String jwtToken;
+
+  /// The amount of time that [jwtToken] will remain valid.
+  ///
+  /// Measured from the time of issue.
   @JsonKey(
     fromJson: durationFromMs,
     toJson: durationToMs,
   )
   final Duration jwtExpiresIn;
+
+  /// A token that can be used to periodically refresh the session.
+  ///
+  /// This value is managed automatically by the [Auth] class.
   final String refreshToken;
+
+  /// The user associated with this session.
   final User user;
 
   /// Multi-factor Authentication information.
@@ -50,18 +66,33 @@ class Session {
   Map<String, dynamic> toJson() => _$SessionToJson(this);
 }
 
+/// Describes an Nhost user.
 @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
 class User {
-  User({this.id, this.displayName, this.email});
+  User({
+    this.id,
+    this.displayName,
+    this.email,
+    this.avatarUrl,
+  });
 
+  /// A GUID identifying the user
   final String id;
+
+  /// The user's preferred name for display
   final String displayName;
+
+  /// The user's email address
   final String email;
+
+  /// A [Uri] locating the user's avatar image, or `null` if none.
+  final Uri avatarUrl;
 
   static User fromJson(dynamic json) => _$UserFromJson(json);
   Map<String, dynamic> toJson() => _$UserToJson(this);
 }
 
+/// Describes information required to perform an MFA login.
 @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
 class MultiFactorAuthenticationInfo {
   MultiFactorAuthenticationInfo({this.ticket});
@@ -75,11 +106,15 @@ class MultiFactorAuthenticationInfo {
   Map<String, dynamic> toJson() => _$MultiFactorAuthenticationInfoToJson(this);
 }
 
+///
 @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
 class MultiFactorAuthResponse {
   MultiFactorAuthResponse({this.qrCode, this.otpSecret});
 
-  /// Base64 data: image of the QR code
+  /// Data URI of the QR code that describes a user's OTP generation.
+  ///
+  /// This value, when presented to the user, can be read by authenticator apps
+  /// to set up OTP.
   @JsonKey(
     name: 'image_url',
     fromJson: uriDataFromString,
