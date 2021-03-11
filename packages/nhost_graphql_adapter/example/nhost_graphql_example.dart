7017c6ff6b441b12e09534b3544ad19a83f334af
Scott Hyndman <scott@madewithfelt.com>
2021-03-11 15:24:31 -0500

    (nhost_graphql_adapter): Scaffold a new package

---
+++ packages/nhost_graphql_adapter/example/nhost_graphql_example.dart
@@ -0,0 +1,6 @@
+import 'package:nhost_graphql/nhost_graphql.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/nhost_graphql_adapter/lib/nhost_graphql.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library nhost_graphql;
+
+export 'src/nhost_graphql_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
