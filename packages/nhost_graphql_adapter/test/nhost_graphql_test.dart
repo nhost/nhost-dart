f0ada03c344a047ba7b40b5b1f1c9678715c54f0
Scott Hyndman <scott@madewithfelt.com>
2021-03-11 15:24:31 -0500

    (nhost_graphql_adapter): Scaffold a new package

---
+++ packages/nhost_graphql_adapter/test/nhost_graphql_test.dart
@@ -0,0 +1,16 @@
+import 'package:nhost_graphql/nhost_graphql.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
