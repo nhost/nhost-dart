192bf03915c4d5ce49ddd1653dedc2404bf75225
Scott Hyndman <scott@madewithfelt.com>
2021-03-13 03:59:45 -0500

    (nhost_sdk): Normalize constructed storage API file paths

--- packages/nhost_sdk/lib/src/foundation/uri.dart
+++ packages/nhost_sdk/lib/src/foundation/uri.dart
@@ -1,12 +1,20 @@
 import 'package:path/path.dart';
 
+final _urlPathContext = Context(style: Style.url);
+
 /// Joins together path fragments
-final _joinPath = Context(style: Style.url).join;
+final _joinPath = _urlPathContext.join;
 
 /// Joins together path fragments so that [basePath] and [subPath] are both
 /// represented in the resulting string, even if [subPath] starts with a `/`.
-String joinSubpath(String basePath, String subPath) =>
-    _joinPath(basePath, subPath.startsWith('/') ? '.$subPath' : subPath);
+String joinSubpath(String basePath, String subPath) {
+  final suffix = subPath.endsWith('/') ? '/' : '';
+  final path = _urlPathContext.normalize(_joinPath(
+    basePath,
+    subPath.startsWith('/') ? '.$subPath' : subPath,
+  ));
+  return '$path$suffix';
+}
 
 extension UriExt on Uri {
   /// Extends a base Uri to include a [subPath], and optionally a new set of
