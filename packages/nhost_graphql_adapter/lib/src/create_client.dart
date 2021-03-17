c11a0dfdb84fd3232ce93876a30dae29039675d2
Scott Hyndman <scott@madewithfelt.com>
2021-03-17 15:00:15 -0400

    (nhost_graphql_adapter): Support overridding the HTTP client during GQL client creation

--- packages/nhost_graphql_adapter/lib/src/create_client.dart
+++ packages/nhost_graphql_adapter/lib/src/create_client.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:graphql/client.dart';
+import 'package:http/http.dart' as http;
 import 'package:nhost_dart_sdk/client.dart';
 
 import 'links.dart';
@@ -10,6 +11,10 @@ import 'links.dart';
 /// The connection will be configured to automatically reflect the logged in
 /// state of [nhostAuth], and will change over time.
 ///
+/// The optional [httpClientOverride] parameter can be provided in order to
+/// customize the requests made by the Nhost APIs, which can be useful for
+/// proxy configuration and debugging.
+///
 /// [defaultRequestHeaders] are an optional map of headers that will accompany
 /// HTTP and web socket requests. Any headers set by the client will overwrite
 /// the default values.
@@ -17,6 +22,7 @@ GraphQLClient createNhostGraphQLClient(
   String nhostGqlEndpointUrl,
   Auth nhostAuth, {
   Map<String, String> defaultRequestHeaders,
+  http.Client httpClientOverride,
 }) {
   return GraphQLClient(
     link: Link.split(
@@ -31,6 +37,7 @@ GraphQLClient createNhostGraphQLClient(
         nhostGqlEndpointUrl,
         nhostAuth,
         defaultHeaders: defaultRequestHeaders,
+        httpClientOverride: httpClientOverride,
       ),
     ),
     cache: GraphQLCache(),
