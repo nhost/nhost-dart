51b24a75aad4383ffba10433072af81214e2c68e
Scott Hyndman <scott@madewithfelt.com>
2021-03-18 09:57:32 -0400

    (nhost_sdk): Add image transformation example

--- packages/nhost_sdk/example/README.md
+++ packages/nhost_sdk/example/README.md
@@ -1,4 +1,5 @@
 # Nhost SDK Examples
 
-* [Authentication API example](https://github.com/nhost/nhost-dart-sdk/example/lib/auth_example.dart): Demonstrates basic use of authentication API
-* [Storage API example](https://github.com/nhost/nhost-dart-sdk/example/lib/storage_example.dart): Demonstrates basic use of authentication API
+* [Authentication API example](https://github.com/nhost/nhost-dart-sdk/example/lib/auth_example.dart): Basic use of the Auth API
+* [Storage API example](https://github.com/nhost/nhost-dart-sdk/example/lib/storage_example.dart): Basic use of the Storage API
+* [Image storage example](https://github.com/nhost/nhost-dart-sdk/example/lib/image_storage_example.dart): Backend image processing example
---
+++ packages/nhost_sdk/example/lib/image_storage_example.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:nhost_dart_sdk/client.dart';
+
+import 'auth_example.dart';
+
+/// Fill in this value with the backend URL found on your Nhost project page.
+const nhostApiUrl = 'https://backend-5e69d1d7.nhost.app';
+
+void main() async {
+  // Setup
+  final client = NhostClient(baseUrl: nhostApiUrl);
+  await loginOrRegister(client,
+      email: 'user-1@nhost.io', password: 'password-1');
+
+  final fileName = 'henry.jpg';
+  final userPath = '/user/${client.auth.currentUser.id}/';
+  final filePath = '$userPath$fileName';
+
+  // Store a new image file...
+  final originalImageBytes = File('./assets/henry.jpg').readAsBytesSync();
+  final imageMetadata = await client.storage.uploadBytes(
+    filePath: filePath,
+    bytes: originalImageBytes,
+    contentType: 'image/jpeg',
+  );
+  print('Uploaded image');
+  print('Size: ${originalImageBytes.length}');
+
+  // ...turn around and download its contents, scaled...
+  final downloadedImage = await client.storage.downloadImage(
+    filePath,
+    fileToken: imageMetadata.nhostMetadata.token,
+    imageTransformConfig: ImageTransformConfig(width: 100, quality: 50),
+  );
+  print('Downloaded transformed image');
+  print('Size: ${downloadedImage.bodyBytes.length}');
+
+  // ...then delete it.
+  await client.storage.delete(filePath);
+
+  // Release
+  client.close();
+}
