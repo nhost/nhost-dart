435305ae99c81d005a073af87ec48480eb565acd
Scott Hyndman <scott@madewithfelt.com>
2021-03-18 08:38:01 -0400

    (nhost_sdk): Clean up examples

--- packages/nhost_sdk/CHANGELOG.md
+++ packages/nhost_sdk/CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.2.0
+
+- Add fileToken support to storage APIs
+- Remove dependency on json_serializable
+- Improve examples
+
 ## 0.1.0
 
 - Add support for auto-login, and externally provided refresh tokens
--- packages/nhost_sdk/example/lib/auth_example.dart
+++ packages/nhost_sdk/example/lib/auth_example.dart
@@ -1,80 +1,38 @@
-import 'dart:io';
-
 import 'package:nhost_dart_sdk/client.dart';
 
+/// Fill in this value with the backend URL found on your Nhost project page.
+const nhostApiUrl = 'https://backend-5e69d1d7.nhost.app';
+
 void main() async {
-  final client = NhostClient(
-    baseUrl: 'https://backend-5e69d1d7.nhost.app',
-  );
-  stdout.write('\nLOGGING IN');
-  waitForReturnKey();
-  var response = await client.auth
-      .login(email: 'scott@madewithfelt.com', password: 'foofoo');
-  print('Success!');
-  print('JWT:\n${response.session.jwtToken}');
+  // Setup
+  final client = NhostClient(baseUrl: nhostApiUrl);
 
-  stdout.write('\nCHANGE PASS');
-  waitForReturnKey();
-  await client.auth
-      .changePassword(oldPassword: 'foofoo', newPassword: 'foofoomcgoo');
-  print('Success!');
+  // Login
+  await loginOrRegister(client,
+      email: 'user-1@nhost.io', password: 'password-1');
 
-  stdout.write('\nLOGOUT (so we can log back in with new pass)');
-  waitForReturnKey();
-  response = await client.auth.logout();
-  print('Session is gone:\n${response.session}');
+  // Print out a few details about the current user
+  final currentUser = client.auth.currentUser;
+  print('currentUser.id: ${currentUser.id}');
+  print('currentUser.displayName: ${currentUser.displayName}');
+  print('currentUser.email: ${currentUser.email}');
 
-  stdout.write('\nLOG BACK IN');
-  waitForReturnKey();
-  response = await client.auth
-      .login(email: 'scott@madewithfelt.com', password: 'foofoomcgoo');
-  print('JWT:\n${response.session.jwtToken}');
-
-  // Roll back password change
-  await client.auth
-      .changePassword(oldPassword: 'foofoomcgoo', newPassword: 'foofoo');
-
-  stdout.write('\nREQUEST EMAIL CHANGE to scotty.hyndman@gmail.com');
-  waitForReturnKey();
-  await client.auth.requestEmailChange(newEmail: 'scotty.hyndman@gmail.com');
-
-  print('Check email for ticket, and enter here:');
-  final ticket = stdin.readLineSync().trim();
-
-  stdout.write('CONFIRM EMAIL CHANGE\n');
-  waitForReturnKey();
-  await client.auth.confirmEmailChange(ticket: ticket);
-  print('Confirmed! New email address!');
-  waitForReturnKey();
-
-  stdout.write('\nHas the local user updated?');
-  waitForReturnKey();
-  print('Nope :(');
-  print('Current user email: ${client.auth.currentUser.email}');
-
-  stdout.write('\nREFRESH SESSION');
-  waitForReturnKey();
-  await client.auth.refreshSession();
-
-  stdout.write('\nAnd now?');
-  waitForReturnKey();
-  print('YES!');
-  print('Current user email: ${client.auth.currentUser.email}');
-
-  stdout.write('\nREGISTER');
-  waitForReturnKey();
-  await client.auth.register(
-    email: 'henry@dogs.com',
-    password: 'abcdefg',
-    defaultRole: 'dog',
-    allowedRoles: ['dog'],
-  );
-  print('Done! currentUser: ${client.auth.currentUser.toJson()}');
+  // And logout
+  await client.auth.logout();
 
   // Release
   client.close();
 }
 
-void waitForReturnKey() {
-  stdin.readLineSync();
+Future<void> loginOrRegister(
+  NhostClient client, {
+  String email,
+  String password,
+}) async {
+  try {
+    return await client.auth.login(email: email, password: password);
+  } on ApiException {
+    // Login failed, so try to register instead
+  }
+  await client.auth.register(email: email, password: password);
 }
