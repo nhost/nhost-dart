439ebe10b6cd02265b12eab2fe3209287cf8e75f
Scott Hyndman <scott@madewithfelt.com>
2021-03-18 05:53:11 -0400

    (nhost_flutter_graphql): Add a simple example that lists todos

--- packages/nhost_flutter_graphql/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/nhost_flutter_graphql/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_macos
+import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
