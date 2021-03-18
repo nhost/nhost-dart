f539004201e8ff832de9ae55a690e23611ad5239
Scott Hyndman <scott@madewithfelt.com>
2021-03-18 08:18:18 -0400

    (nhost_sdk): Structure example similarly to other packages
    README pointing to various examples, by full URL

--- packages/nhost_sdk/example/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:nhost_dart_sdk/client.dart';
-
-void main() async {
-  final nhost = NhostClient(
-    baseUrl: 'https://backend-5e69d1d7.nhost.app',
-  );
-  await nhost.auth.login(email: 'scott@madewithfelt.com', password: 'foofoo');
-  await nhost.storage.getDirectoryMetadata('/public/');
-}
