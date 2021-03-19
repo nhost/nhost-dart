967fb27af3479493ceb1b1db6e7e4b867edf39ee
Scott Hyndman <scott@madewithfelt.com>
2021-03-19 17:43:59 -0400

    (nhost_graphql_adapter): Rename and clean up the todo example

--- packages/nhost_graphql_adapter/example/lib/todo_example.dart
+++ packages/nhost_graphql_adapter/example/lib/todo_example.dart
@@ -1,35 +1,41 @@
+/// A simple example that queries the "todos" table created in the Nhost Quick
+/// Start (https://docs.nhost.io/quick-start).
+library simple_example;
+
 import 'package:graphql/client.dart';
 import 'package:nhost_dart_sdk/client.dart';
 import 'package:nhost_graphql_adapter/nhost_graphql_adapter.dart';
 
 // Both of these URLs can be found in your nhost.io console, under the project
 // you wish to connect to.
+
 const backendEndpoint = 'https://backend-5e69d1d7.nhost.app';
 const graphQLEndpoint = 'https://hasura-5e69d1d7.nhost.app/v1/graphql';
+
+final todosQuery = gql(r'''
+  query {
+    todos {
+      id
+      name
+      is_completed
+      user_id
+      created_at
+      updated_at
+    }
+  }
+''');
 
 void main() async {
   // Set up the Nhost and GraphQL clients
   final nhostClient = NhostClient(baseUrl: backendEndpoint);
   final graphqlClient = createNhostGraphQLClient(
     graphQLEndpoint,
-    nhostClient.auth,
+    nhostClient,
   );
-
-  final myTodosQuery = gql(r'''
-    query {
-      todos {
-        id
-        name
-        is_completed
-        created_at
-        updated_at
-      }
-    }
-  ''');
 
   // Run a query, unauthenticated
   var queryResult = await graphqlClient.query(
-    QueryOptions(document: myTodosQuery),
+    QueryOptions(document: todosQuery),
   );
 
   // This failed, because we're not authenticated
@@ -41,11 +47,10 @@ void main() async {
 
   // ...and try again, authenticated
   queryResult = await graphqlClient.query(
-    QueryOptions(document: myTodosQuery),
+    QueryOptions(document: todosQuery),
   );
 
   // Success!
-  print(queryResult.exception);
   print(queryResult.data['todos']);
 
   nhostClient.close();
