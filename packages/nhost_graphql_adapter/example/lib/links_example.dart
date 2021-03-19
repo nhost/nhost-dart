3f33cd95eb97317d53a0601e9c287ab48ec74784
Scott Hyndman <scott@madewithfelt.com>
2021-03-19 17:44:19 -0400

    (nhost_graphql_adapter): Add an example demonstrating custom link creation

---
+++ packages/nhost_graphql_adapter/example/lib/links_example.dart
@@ -0,0 +1,68 @@
+/// Demonstrates how to construct your own client by composing custom [Link]s
+/// along with the links provided by this package.
+library links_example;
+
+import 'dart:async';
+
+import 'package:graphql/client.dart';
+import 'package:nhost_dart_sdk/client.dart';
+import 'package:nhost_graphql_adapter/nhost_graphql_adapter.dart';
+
+import 'todo_example.dart';
+
+// Both of these URLs can be found in your nhost.io console, under the project
+// you wish to connect to.
+
+const backendEndpoint = 'https://backend-5e69d1d7.nhost.app';
+const graphQLEndpoint = 'https://hasura-5e69d1d7.nhost.app/v1/graphql';
+
+final myTodosQuery = gql(r'''
+  query {
+    todos {
+      id
+      name
+      is_completed
+      user_id
+      created_at
+      updated_at
+    }
+  }
+''');
+
+void main() async {
+  final nhostClient = NhostClient(baseUrl: backendEndpoint);
+
+  // The Nhost "terminating link" (the point at which requests are sent). We're
+  // going to build links that execute on requests before this point is reached.
+  final nhostLink = combinedLinkForNhost(
+    graphQLEndpoint,
+    nhostClient,
+  );
+
+  // Create a new custom link that logs all requests and responses passing
+  // through it
+  final loggingMiddleware = Link.function((request, [nextLink]) {
+    print('REQUEST: $request');
+    return nextLink(request).transform(
+      StreamTransformer.fromHandlers(
+        handleData: (data, sink) {
+          print('RESPONSE: $data');
+          sink.add(data);
+        },
+      ),
+    );
+  });
+
+  // Construct a GraphQL client using the composed link
+  final gqlClient = GraphQLClient(
+    link: loggingMiddleware.concat(nhostLink),
+    cache: GraphQLCache(),
+  );
+
+  // Now we query, and will see logs printed to the console
+  await gqlClient.query(QueryOptions(
+    document: todosQuery,
+  ));
+
+  nhostClient.close();
+}
