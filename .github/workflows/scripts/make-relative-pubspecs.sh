69b4df7f2f9816228888e2e945fcc897c2658164
Scott Hyndman <scott@madewithfelt.com>
2021-03-24 05:52:48 -0400

    ci(repo): Change pubspec to relative paths before pana

---
+++ .github/workflows/scripts/make-relative-pubspecs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+script_dir=`dirname "$BASH_SOURCE"`
+repo_dir=`realpath "$script_dir/../../.."`
+
+get_packages () {
+ ls -1 $repo_dir/packages
+}
+
+nl=$'\n'
+for target_package in $(get_packages); do
+  pushd $repo_dir/packages/$target_package
+    for dependency_package in $(get_packages); do
+      if [ $target_package == $dependency_package ]; then
+        continue;
+      fi
+
+      if egrep "$dependency_package" pubspec.yaml; then
+        cat pubspec.yaml | \
+          sed "s/$dependency_package:.*/$dependency_package:\\${nl}    path: ..\/$dependency_package\//" > pubspec.new
+
+        mv pubspec.new pubspec.yaml
+      fi
+    done
+  popd
+done
