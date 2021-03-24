a08c97349d3be36acb9e1c713c546821add26235
Scott Hyndman <scott@madewithfelt.com>
2021-03-24 04:05:28 -0400

    fix(nhost_flutter_auth): Fix the OAuth provider login flow example

--- packages/nhost_flutter_auth/example/lib/oauth_providers_example.dart
+++ packages/nhost_flutter_auth/example/lib/oauth_providers_example.dart
@@ -6,8 +6,10 @@
 /// GitHub OAuth application using the instructions at
 /// https://docs.nhost.io/auth/oauth-providers/github
 ///
-/// Then, in your Nhost project's "Sign-In" settings, set the redirect URL to
-/// `nhost://open.example.auth.app`.
+/// Then, in your Nhost project's "Sign-In" settings, set:
+///
+/// Success redirect URL: `nhost-example://oauth.login.success`.
+/// Failure redirect URL: `nhost-example://oauth.login.failure`.
 library oauth_providers_example;
 
 import 'package:flutter/material.dart';
@@ -15,8 +17,14 @@ import 'package:nhost_flutter_auth/nhost_flutter_auth.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
 import 'package:app_links/app_links.dart';
 
+import 'simple_auth_example.dart';
+
 /// Fill in this value with the backend URL found on your Nhost project page.
 const nhostApiUrl = 'https://backend-5e69d1d7.nhost.app';
+const nhostGithubLoginUrl = '$nhostApiUrl/auth/providers/github/';
+
+const loginSuccessHost = 'oauth.login.failure';
+const loginFailureHost = 'oauth.login.failure';
 
 void main() async {
   runApp(OAuthExample());
@@ -40,7 +48,10 @@ class _OAuthExampleState extends State<OAuthExample> {
 
     appLinks = AppLinks(
       onAppLink: (uri) async {
-        print('!!!!! $uri');
+        if (uri.host == loginSuccessHost) {
+          // ignore: unawaited_futures
+          nhostClient.auth.completeOAuthProviderLogin(uri);
+        }
         await url_launcher.closeWebView();
       },
     );
@@ -60,19 +71,54 @@ class _OAuthExampleState extends State<OAuthExample> {
         title: 'Nhost.io OAuth Example',
         home: Scaffold(
           body: SafeArea(
-            child: TextButton(
-              onPressed: () async {
-                await url_launcher.launch(
-                  'https://backend-5e69d1d7.nhost.app/auth/providers/github/',
-                  forceSafariVC: true,
-                );
-              },
-              child: Text('Authenticate with GitHub'),
-            ),
+            child: ExampleProtectedScreen(),
           ),
           // ExampleProtectedScreen(),
         ),
       ),
+    );
+  }
+}
+
+class ExampleProtectedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // NhostAuthProvider.of will register this widget so that it rebuilds whenever
+    // the user's authentication state changes.
+    final auth = NhostAuthProvider.of(context);
+    Widget widget;
+
+    switch (auth.authenticationState) {
+      case AuthenticationState.loggedIn:
+        widget = LoggedInUserDetails();
+        break;
+      default:
+        widget = ProviderLoginForm();
+        break;
+    }
+
+    return Padding(
+      padding: EdgeInsets.all(32),
+      child: widget,
+    );
+  }
+}
+
+class ProviderLoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        try {
+          await url_launcher.launch(
+            nhostGithubLoginUrl,
+            forceSafariVC: true,
+          );
+        } on Exception {
+          // This can occur if the user
+        }
+      },
+      child: Text('Authenticate with GitHub'),
     );
   }
 }
