bbad49a61aa698492828c884687baf6eccd58ee2
Scott Hyndman <scotty.hyndman@gmail.com>
2021-06-16 15:18:35 -0400

    chore: Update pod deps for an example app

--- packages/nhost_flutter_graphql/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/nhost_flutter_graphql/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_macos
+import connectivity_plus_macos
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
