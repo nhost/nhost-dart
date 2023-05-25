8744d5ce38d76d324ca16304b98a07071c313f6d
AlexMichels <michels@dialogplus.nrw>
2023-05-25 15:29:36 +0700

    refactor nhost_sdk

--- packages/nhost_sdk/lib/nhost_sdk.dart
+++ packages/nhost_sdk/lib/nhost_sdk.dart
@@ -5,6 +5,7 @@ export 'src/api/api_client.dart' show ApiException, ApiClient;
 export 'src/api/auth_api_types.dart';
 export 'src/api/subdomain.dart';
 export 'src/api/service_urls.dart';
+export 'src/api/core_codec.dart';
 export 'src/http.dart';
 export 'src/errors.dart';
 export 'src/base/base.dart';
