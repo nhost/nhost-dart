72bb25af8bfb5e506d6971c10dbb9efc2044d113
Hassan Ben Jobrane <hsanbenjobrane@gmail.com>
2023-10-31 19:41:46 +0100

    chore: remove old testing project

--- packages/nhost_dart/test/server/functions/test_function.js
+++
@@ -1,9 +0,0 @@
-export default (req, res) => {
-  const receivedArgs = [];
-  if (req.body.arg) receivedArgs.push(req.body.arg);
-  if (req.query.arg) receivedArgs.push(req.query.arg);
-
-  res
-    .status(200)
-    .send({ receivedArgs });
-};
--- packages/nhost_dart/test/server/nhost/migrations/default/1643347087950_set_fk_storage_files_uploaded_by_user_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "storage"."files" drop constraint "files_uploaded_by_user_id_fkey";
