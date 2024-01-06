48ac3cc606c61c386706723ef064783a4b18f2a4
fattabe <fattabe@hilti.com>
2024-01-07 00:50:04 +0300

    Upgrade dependencies & Dart SDK

--- packages/nhost_dart/test/matchers.dart
+++ packages/nhost_dart/test/matchers.dart
@@ -1,4 +1,4 @@
-import 'package:test_api/expect.dart';
+import 'package:test/expect.dart';
 
 /// Matches when the provided iterable is monotonically increasing.
 const isIncreasing = _IsIncreasingMatcher();
